5f52d6dd5255c7a0e24e6e00bf05a206beccf029
HunnyMal <97999452+HunnyMal@users.noreply.github.com>
2022-02-21 12:08:53 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        GuessNumber obj = new GuessNumber();
+        obj.inputNumbers();
+        obj.analyzeGuess();
+    }
+}
