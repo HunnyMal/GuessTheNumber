05898682d010e762d68abbff69fff459c942c4ec
HunnyMal <97999452+HunnyMal@users.noreply.github.com>
2022-02-22 14:26:21 +0800

    Added Documetation for Main.java
    Added JavaDoc comments inside the source code of Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
-import java.util.Scanner;
 import java.awt.*;
 
+/**
+ * 
+ * @author HunnyMal
+ */
 public class Main {
+
+    /**
+     * @param args the command line arguments
+     */    
     public static void main(String[] args) {
-        GuessNumber obj = new GuessNumber();
-        obj.inputNumbers();
-        obj.analyzeGuess();
+        GuessNumber obj = new GuessNumber(); //instance of GuessNumber to call methods
+        obj.inputNumbers(); //method to enable user to set maximum range (0 - x);
+                            //and determine correct number to guess
+        obj.analyzeGuess(); //allows user to input number as guess
+                            //and check if your guess is correct
     }
 }
