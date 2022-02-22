9edcc3e67d3976f6ea2ee33eb242d642edea5eb5
HunnyMal <97999452+HunnyMal@users.noreply.github.com>
2022-02-22 14:24:28 +0800

    Add First Documentation for GuessNumber.java
    Added Javadoc comments inside the source code

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,70 +1,85 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author HunnyMal
+ */
 public class GuessNumber {
-    //Attributes
-    private int maxNum, 
-                correctNumber, guessNumber,
-                scoreNumber;
+    /**
+     * Private attributes of class GuessNumber()
+     */
+    private int maxNum,     //maximum range of the number to guess ( 0 to (maxNum) )
+            correctNumber,  //single number to guess within the range of 0 to (maxNum)
+            guessNumber,    //input of user as guess for value of (correctNumber)
+            scoreNumber;    //attempts made by user to guess (correctNumber)
 
-    //Constructor
+    /**
+     * Constructor for GuessNumber() with predetermined values of maxNum, scoreNumber;
+     * and prints on screen an introductory message
+     */
     GuessNumber() {
         maxNum = 100;
         scoreNumber = 0;
         System.out.println("Welcome to Number Guesser Written by HunnyMal.");
     }
-
-    //GETTERS AND SETTERS
-    //maxNum
-    public int getMaxNum() {
-        return this.maxNum;
-    }
-    public void setMaxNum(int max) {
+     /**
+      * Getters and Setters of GuessNumber() class attributes
+      */
+    public void setMaxNum(int max) {        //public setter method for maxNum
         this.maxNum = max; 
     }
-    //correctNumber
-    private void setCorrectNumber() {
+    private int getMaxNum() {               //private getter method for maxNum
+        return this.maxNum; 
+    } 
+    private void setCorrectNumber() {       //private setter method for correctNumber
         Random randNum = new Random();
-        this.correctNumber = randNum.nextInt(maxNum + 1);
+        this.correctNumber = randNum.nextInt(getMaxNum() + 1); 
     }
-    public int getCorrectNumber() {
-        return this.correctNumber;
+    public int getCorrectNumber() {         //public getter method for correctNumber
+        return this.correctNumber; 
     }
-    //guessNumber
-    private void setGuessNumber(int num) {
-        this.guessNumber = num;
+    private void setGuessNumber(int num) {  //private setter method for guessNumber
+        this.guessNumber = num; 
     }
-    public int getGuessNumber() {
-        return this.guessNumber;
+    public int getGuessNumber() {           //public getter method for guessNumber
+        return this.guessNumber; 
     }
-    //scoreNumber
-    private void setScore(int num) {
-        this.scoreNumber = num;
+    private void setScore(int num) {        //private setter method for scoreNumber
+        this.scoreNumber = num; 
     }
-    public int getScore() {
+    public int getScore() {                 //public getter method for scoreNumber
         return this.scoreNumber;
     }
 
-    //Methods
-    Scanner scanInt = new Scanner(System.in);
+    Scanner scanInt = new Scanner(System.in);   //instance of Scanner for local usage 
+                                                //(creates bugs if multiple instances are created in each methods)
 
+    /**
+     * Method to allow user to input the maximum number range, and 
+     * determines the value of the correctNumber within 0 to maxNum; 
+     * while loop creates instance of Scanner for input, and ensures input value is more than 0;
+     * uses setMaxNum(int num) to set value of maxNum based on input;
+     * uses setCorrectNumber() to set value of correctNumber 
+     */
     public void inputNumbers() {
-        //maxNum
         int max = 0;
         while (max <= 0) {
             System.out.println("Input the Maximum Number Range (Enter a Real Number from 0 onwards): ");
             max = scanInt.nextInt();
         }
         setMaxNum(max);
-        //scanInt.close();
-        //correctNumber
-        setCorrectNumber();
-        //guessNumber
-        
+        setCorrectNumber();     
     }
 
+    /**
+     * method to receive guessNumber input and analyze if it matches with value of correctNumber;
+     * uses while loop to analyze input if correct, 
+     * ensure Scanner instance is made to allow user input if it does not match condition, 
+     * increment and print value of scoreNumber,
+     * prints different String depending if condition is met/unmet
+     */
     public void analyzeGuess() {
-        //Scanner scanGuess = new Scanner(System.in);
         System.out.println("Input Your Guess: ");
         while (this.guessNumber != correctNumber) {
             int guess = scanInt.nextInt();
